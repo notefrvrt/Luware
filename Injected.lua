163de4643a8acf0dae1339cbbf95b07c288e9aae
notefrvrt <nk228.555.az@gmail.com>
2024-10-21 20:07:08 +0900

    Update Injected.lua

--- Injected.lua
+++ Injected.lua
@@ -1,8 +1,8 @@
 _G.initialized = true
 _G.Injected = true
 game.StarterGui:SetCore("SendNotification", {
-    Title = "TrianAPI";
-    Text = "Attached to game";
-    Duration = "5";
-    Callback = NotificationBindable;
+    Title = "TrianAPI",
+    Text = "Attached to game",
+    Duration = "10",
+	Button1 = "Ok"
 })
